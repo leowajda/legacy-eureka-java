e07f1b78d6bcee292c23dc28cd59f306bbe25ad7
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-16 08:38:18 +0200

    :fire: Remove unused import

--- src/test/java/singly_linked_list/iterative/LC_0002Test.java
+++ src/test/java/singly_linked_list/iterative/LC_0002Test.java
@@ -1,7 +1,6 @@
 package singly_linked_list.iterative;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
