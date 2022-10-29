e40e59171b534b06da03f4494f0bc1d63b07c4fa
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-29 11:03:53 +0200

    :sparkles: Add iterative solution for 'Fibonacci Number'

---
+++ src/main/java/math/iterative/LC_0509.java
@@ -0,0 +1,19 @@
+package math.iterative;
+
+public class LC_0509 {
+
+    public int fib(int n) {
+        if (n == 0 || n == 1)
+            return n;
+
+        int a = 0, b = 1, c;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
