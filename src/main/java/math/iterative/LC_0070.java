400db1a40bf770e65d67f20a3b0a0bb77503c9d1
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-20 10:04:37 +0100

    :sparkles: Add iterative solution for 'Climbing Stairs'

---
+++ src/main/java/math/iterative/LC_0070.java
@@ -0,0 +1,18 @@
+package math.iterative;
+
+public class LC_0070 {
+    public int climbStairs(int n) {
+
+        if (n <= 2)
+            return n;
+
+        int a = 1, b = 2, c;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
