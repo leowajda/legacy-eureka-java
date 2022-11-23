b68b78d03f982e15764a7dbef2cdc4104a447eee
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-23 15:29:05 +0100

    :white_check_mark: Add test for iterative 'Valid Sudoku'

---
+++ src/test/java/array/iterative/LC_0036Test.java
@@ -0,0 +1,59 @@
+package array.iterative;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class LC_0036Test {
+
+    private LC_0036 solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new LC_0036();
+    }
+
+    private static Stream<Arguments> testIsValidSudoku() {
+        char[][] a = new char[][] {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        char[][] b = new char[][] {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        return Stream.of(arguments(a, true), arguments(b, false));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource
+    public void testIsValidSudoku(char[][] board, boolean expected) {
+        boolean result = solution.isValidSudoku(board);
+        assertThat(result, is(equalTo(expected)));
+    }
+}
