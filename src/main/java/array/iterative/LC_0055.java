f41f6ef6322005f6af81348561dee8ec0b756846
Leonardo Wajda <dev.leowajda@tuta.io>
2022-12-16 17:21:46 +0100

    :sparkles: Add iterative solution for 'Jump Game'

---
+++ src/main/java/array/iterative/LC_0055.java
@@ -0,0 +1,14 @@
+package array.iterative;
+
+public class LC_0055 {
+    public boolean canJump(int[] nums) {
+
+        int last = nums.length - 1;
+
+        for (int i = last; i >= 0; i--)
+            if (i + nums[i] >= last)
+                last = i;
+
+        return last == 0;
+    }
+}
