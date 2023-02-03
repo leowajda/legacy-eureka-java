49d207e22d448911a5fa3d413f12733533a76559
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-03 13:58:50 +0100

    :zap: Refactor nested loop

--- src/main/java/string/iterative/LC_0091.java
+++ src/main/java/string/iterative/LC_0091.java
@@ -6,21 +6,21 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toSet;
 
 public class LC_0091 {
-
-    private final static Set<String> nums = IntStream.rangeClosed(1, 26).mapToObj(String::valueOf).collect(toSet());
-
     public int numDecodings(String s) {
 
-        int n = s.length();
-        int[] memo = new int[n];
+        Set<String> nums = IntStream.rangeClosed(1, 26).mapToObj(String::valueOf).collect(toSet());
+        int[] memo = new int[s.length() + 1];
 
-        for (int i = n - 1; i >= 0; i--)
-            for (int j = i + 1; j <= n; j++) {
-                String sub = s.substring(i, j);
-                if (nums.contains(sub))
-                    memo[i] += (j == n) ? 1 : memo[j];
-            }
+        memo[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
 
-        return memo[0];
+            if (nums.contains(s.substring(i - 1, i)))
+                memo[i] += memo[i - 1];
+
+            if (i >= 2 && nums.contains(s.substring(i - 2, i)))
+                memo[i] += memo[i - 2];
+        }
+
+        return memo[s.length()];
     }
 }
