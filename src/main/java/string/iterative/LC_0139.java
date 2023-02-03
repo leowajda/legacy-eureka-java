8809ceea35e3551c4954aabd56b2571290321eab
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-03 11:08:56 +0100

    :sparkles: Add iterative solution for 'Word Break'

---
+++ src/main/java/string/iterative/LC_0139.java
@@ -0,0 +1,23 @@
+package string.iterative;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LC_0139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] memo = new boolean[s.length() + 1];
+
+        memo[0] = true;
+        for (int i = 1; i <= s.length(); i++)
+            for (int j = 0; j < i; j++)
+                if (words.contains(s.substring(j, i)) && memo[j]) {
+                    memo[i] = true;
+                    break;
+                }
+
+        return memo[s.length()];
+    }
+}
